42751aa1e6c6442f3be9973fea824b504064c0f1
deepak <d.ranolia92@gmail.com>
2020-09-07 05:24:47 +0530

    legacy code 1.1

--- signup/signup.module.ts
+++ signup/signup.module.ts
@@ -15,5 +15,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     ReactiveFormsModule,
   ]
 })
-export class SignupModule { }
-// export class SignupModule { }
+export class SignupModule {
+  
+}
